7d8fdb821585d1e9ca85c8a2ae338a7249215d3e
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-06 12:26:01 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,7 +2,9 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(info_hash)
-  	info_hash.each{|key, value| self.send(("#{key}="), value)}
+  def initialize(name, breed, age)
+  	@name = name
+    @breed = breed
+    @age = age
   end
 end
