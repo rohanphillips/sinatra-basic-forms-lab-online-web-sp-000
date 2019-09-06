a929f212261498f524e24b0257a5f6dc035cb7e3
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-06 12:16:09 +0000

    Automatically backed up by Learn

---
+++ models/puppy.rb
@@ -0,0 +1,10 @@
+
+class puppies
+  attr_accessor :name, :breed, :months_old
+
+  def initialize(name, breed, months_old)
+    @name = name
+    @breed = breed
+    @months_old = months_old
+  end
+end
