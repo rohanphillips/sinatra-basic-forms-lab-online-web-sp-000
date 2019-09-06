7cca7ead3ba2b16a533cf2ee46227ffbbc9f8370
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-06 12:27:10 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 
 class Puppy
-  attr_accessor :name, :breed, :age
+  attr_accessor :name, :breed, :months_old
 
   def initialize(name, breed, age)
   	@name = name
     @breed = breed
-    @age = age
+    @months_old = age
   end
 end
